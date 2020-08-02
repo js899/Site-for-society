33803d5b6c1a73f2ac0aa9cf097301be2198bf59
js899 <49071268+js899@users.noreply.github.com>
2020-08-03 02:10:05 +0530

    Update settings.py

--- csi/settings.py
+++ csi/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['code.gndec.ac.in','127.0.0.1', 'localhost']
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jaideepjssingh@gmail.com'
-EMAIL_HOST_PASSWORD = 'pajasGMmanar*99'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
