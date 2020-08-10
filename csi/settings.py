654c5866b71db6879a3f9f16bf0e98d09cd53a86
js899 <jaideepjssingh@gmail.com>
2020-08-10 23:45:09 +0530

    Disabled a field

--- csi/settings.py
+++ csi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_forms',
 #    'django.contrib.sites',
 #    'sorl.thumbnail',
+    'widget_tweaks',
     'newsletters',
     'django.contrib.admin',
     'django.contrib.auth',
